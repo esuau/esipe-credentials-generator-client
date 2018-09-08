190e064994970075bc11e046a9cfeda837d2f6a6
esuau <evan.suau@etu.u-pec.fr>
2018-09-08 22:37:57 +0200

    Create primary design frame

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +15,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
