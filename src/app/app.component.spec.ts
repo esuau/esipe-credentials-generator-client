6117f7c6cbe063c5f9c7cf78be05f412422b6a63
esuau <evan.suau@etu.u-pec.fr>
2018-09-10 23:26:04 +0200

    Test email input field

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,10 +2,12 @@ import { TestBed, async } from '@angular/core/testing';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
 import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
 import { EmailComponent } from './components/email/email.component';
 import { PasswordComponent } from './components/password/password.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -18,7 +20,8 @@ describe('AppComponent', () => {
         AppRoutingModule,
         FlexLayoutModule,
         MatButtonModule,
-        MatCardModule
+        MatCardModule,
+        MatInputModule
       ]
     }).compileComponents();
   }));
--- src/app/components/email/email.component.spec.ts
+++ src/app/components/email/email.component.spec.ts
@@ -1,10 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+
+import { AppRoutingModule } from '../../app.routing';
 
 import { EmailComponent } from './email.component';
+import { PasswordComponent } from '../password/password.component';
 
 describe('EmailComponent', () => {
   let component: EmailComponent;
@@ -12,11 +18,19 @@ describe('EmailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EmailComponent ],
+      declarations: [
+        EmailComponent,
+        PasswordComponent
+      ],
       imports: [
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        FormsModule,
         FlexLayoutModule,
         MatButtonModule,
-        MatCardModule
+        MatCardModule,
+        MatInputModule,
+        ReactiveFormsModule
       ]
     })
     .compileComponents();
@@ -30,5 +44,17 @@ describe('EmailComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have email field valid with UPEC address', () => {
+    component.emailFormControl.setValue('email@u-pec.fr');
+    expect(component.emailFormControl.valid).toEqual(true);
+    component.emailFormControl.setValue('email@etu.u-pec.fr');
+    expect(component.emailFormControl.valid).toEqual(true);
+  });
+
+  it('should have email field invalid with non-UPEC address', () => {
+    component.emailFormControl.setValue('email@domain.com');
+    expect(component.emailFormControl.valid).toEqual(false);
   });
 });
