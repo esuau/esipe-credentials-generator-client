7bce3eb5f0d1318e4155330e4ddd9420cc4ba826
esuau <evan.suau@etu.u-pec.fr>
2018-11-17 15:49:03 +0100

    test(karma): Run unit tests in Travis CI

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -24,8 +24,12 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    }
   });
 };
