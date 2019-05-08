8a1637f38b423a195a268c4c3dc6c0bfad0c3af1
esuau <evan.suau@etu.u-pec.fr>
2019-05-08 14:05:43 +0200

    chore(ci): Version npm package and Docker image

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+npm version patch -m "Release new version"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push $DOCKER_USERNAME/esipe-credentials-client
+docker push $DOCKER_USERNAME/esipe-credentials-client:$(grep 'version' package.json | cut -d '"' -f4)
